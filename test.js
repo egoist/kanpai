7d4f42aa03e297b0f73c0045ee960ca65ff40ec8
EGOIST <0x142857@gmail.com>
2016-02-18 11:10:32 +0800

    clean

--- test.js
+++
@@ -1,4 +0,0 @@
-const spawnSync = require('child_process').spawnSync
-
-const a = spawnSync('git', ['tag'])
-console.log(a.stdout.toString().trim() && a.stdout.toString().trim().split('\n').map(v => v.substr(1)))
